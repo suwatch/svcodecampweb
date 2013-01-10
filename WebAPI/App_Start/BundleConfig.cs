aa62b52a625858dcf8aa56741fbd70983ac84e85
elwhizard <elwhizard@gmail.com>
2013-01-10 11:10:06 +0800

    First push for SVCC 2013 site - Page structure - CSS and SASS - Images and sprites

--- WebAPI/App_Start/BundleConfig.cs
+++ WebAPI/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WebAPI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Styles/svcc").Include("~/Content/Styles/svcc.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",

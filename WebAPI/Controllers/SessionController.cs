85198dd67c3b76db0802f2c1332858a1ff343850
elwhizard <elwhizard@gmail.com>
2013-01-11 13:11:37 +0800

    Left sidebar layout and css updates

--- WebAPI/Controllers/SessionController.cs
+++ WebAPI/Controllers/SessionController.cs
@@ -49,7 +49,7 @@ namespace WebAPI.Controllers
 
             var viewModel = new SessionViewModel()
                                 {
-                                    DaysUntilCodeCampString = "9999 Days Until Camp",
+                                    DaysUntilCodeCampString = "9999",
                                     Sessions = sessions.OrderBy(a => a.SessionSlug).ToList(),
                                     Sponsors = sponsors
                                 };

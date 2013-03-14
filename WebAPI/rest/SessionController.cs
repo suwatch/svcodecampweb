159fc6416e944fd059fcfb6c4105fb8fdf84c4a1
pkellner <peter@peterkellner.net>
2013-03-14 07:35:39 -0700

    wip

--- WebAPI/rest/SessionController.cs
+++ WebAPI/rest/SessionController.cs
@@ -10,6 +10,47 @@ namespace WebAPI.REST
 {
     public class SessionController : ApiController
     {
+
+        /// <summary>
+        /// route like implementation of Get.  If no parameters then get all sessions.  
+        /// option = BySpeaker with param1 = "#" gives all sessions for given speaker
+        /// </summary>
+        /// <param name="option"></param>
+        /// <param name="param1"></param>
+        /// <param name="param2"></param>
+        /// <param name="param3"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Get(string option,string param1,string param2,string param3)
+        {
+            SessionsQuery sessionQuery = null;
+           
+
+            if (String.IsNullOrEmpty(option))
+            {
+                sessionQuery = new SessionsQuery();
+            }
+            else if (option.ToLower().Equals("byspeaker") && !String.IsNullOrEmpty(param1))
+            {
+                int attendeesId;
+                if (!Int32.TryParse(param2, out attendeesId))
+                {
+                    attendeesId = -2;
+                }
+                sessionQuery = new SessionsQuery
+                                   {
+                                       Attendeesid = attendeesId
+                                   };
+            }
+
+            var session = new List<SessionsResult>();
+            if (sessionQuery != null)
+            {
+                session = SessionsManager.I.Get(sessionQuery);
+            }
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, session);
+            return response;
+        }
        
 
         public HttpResponseMessage Get(int id)
